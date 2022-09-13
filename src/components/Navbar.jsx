cc34e8bfd2d8497252a59e06326cf8de45f5a78f
LiMi <75808911+LiMi0000@users.noreply.github.com>
2022-09-13 18:57:57 +0200

    navbar done - starting with home components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
+import { links } from '../constants/links';
+
 const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      link: 'home',
-    },
-    {
-      id: 2,
-      link: 'about',
-    },
-    {
-      id: 3,
-      link: 'portfolio',
-    },
-    {
-      id: 4,
-      link: 'experience',
-    },
-    {
-      id: 5,
-      link: 'contact',
-    },
-  ];
+  const [navbar, setNavbar] = useState(false);
+
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white fixed bg-black">
-      <div className="text-5xl font-signature ml-2">Çlirim</div>
+      <div className="text-5xl font-signature ml-2 ">Çlirim Misini</div>
 
       <ul className="hidden md:flex">
         {links.map(({ link, id }) => (
@@ -38,6 +20,25 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div
+        onClick={() => setNavbar(!navbar)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 hover:text-white duration-200 md:hidden"
+      >
+        {navbar ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {navbar && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+          {links.map(({ link, id }) => (
+            <li
+              key={id}
+              className="text-gray-500 hover:text-white duration-200 cursor-pointer hover:scale-105 px-4 capitalize text-4xl py-6"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
